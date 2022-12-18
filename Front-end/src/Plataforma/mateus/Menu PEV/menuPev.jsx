3b24adb414f9906728d2a81f8208c9e013916368
Mateus Abdias <mateusabdias.dev@gmail.com>
2022-12-18 02:01:59 -0300

    Atualizando front

---
+++ Front-end/src/Plataforma/mateus/Menu PEV/menuPev.jsx
@@ -0,0 +1,28 @@
+export default function MenuPev() {
+  return (
+    <div>
+      <img
+        className="menubento"
+        src="https://github.com/MateusAbdias/imagensConectaRecycle/blob/main/plataforma/147ec461-cdc1-49ba-98cb-ad7e9b120732.png?raw=true"
+      />
+      <img
+        className="logo"
+        src="https://github.com/MateusAbdias/imagensConectaRecycle/blob/main/logo.png?raw=true"
+      />
+      <img className="notificacao" src="https://github.com/MateusAbdias/imagensConectaRecycle/blob/main/plataforma/61b07a6c-d831-4451-abf5-d8a9fb83e678.png?raw=true" alt="" />
+      <div className="main">
+        <h2>Bem vindo Green City</h2>
+        <p>O que você quer fazer hoje?</p>
+
+        <div>
+          <ul>
+            <li><a href="#"><img className="card card1" src="https://github.com/MateusAbdias/imagensConectaRecycle/blob/main/plataforma/menu%20(1).png?raw=true"/></a></li>
+            <li><a href="#"><img className="card" src="https://github.com/MateusAbdias/imagensConectaRecycle/blob/main/plataforma/menu%20(2).png?raw=true"/></a></li>
+            <li><a href="#"><img className="card" src="https://github.com/MateusAbdias/imagensConectaRecycle/blob/main/plataforma/menu%20(3).png?raw=true"/></a></li>
+          </ul>
+        </div>
+
+      </div>
+    </div>
+  );
+}
